0864c27ae93efff2cfcb961031db9e646f2dea8c
joltzz <guijsilva02@gmail.com>
2022-05-11 18:47:39 -0300

    single product page ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { GlobalStyle } from "./styles/style";
-import { MainPage, Login, SignUp } from "./pages";
-import AuthContext from "./context/authContext";
+import { MainPage, Login, SignUp, SingleProduct} from "./pages";
+import AuthContext from "./context/AuthContext";
+import CartContext from "./context/CartContext";
 
-import { GlobalStyle } from "./styles/style";
 
 export default function App() {
     const [token, setToken] = useState(null);
+    const [cartQuantity, setCartQuantity] = useState(0);
 
     return (
         <>
             <GlobalStyle />
             <BrowserRouter>
+            <CartContext.Provider value={{ cartQuantity, setCartQuantity }}>
                 <AuthContext.Provider value={{ token, setToken }}>
                     <Routes>
                         <Route path="/" element={<MainPage />} />
                         <Route path="/login" element={<Login />} />
                         <Route path="/sign-up" element={<SignUp />} />
+                        <Route path="/product/:productId" element={<SingleProduct />} />
                     </Routes>
                 </AuthContext.Provider>
+                </CartContext.Provider>
             </BrowserRouter>
         </>
     )
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,89 +5,117 @@ import {
     Button,
     StyledLink,
     Title,
-} from "../../components/Form";
-
-import { ThreeDots } from "react-loader-spinner";
-import { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-
-import Swal from "sweetalert2";
-import LogoContainer from "../../components/Form/LogoContainer";
-import Footer from "../../components/Form/Footer";
-
-import AuthContext from "../../context/authContext";
-import Logo from "../../assets/img/Logo.png"
-
-export default function Login() {
+  } from "../../components/Form";
+  
+  import { ThreeDots } from "react-loader-spinner";
+  import { useState, useEffect } from "react";
+  import { useNavigate } from "react-router-dom";
+  
+  import Swal from "sweetalert2";
+  import api from "../../services/api";
+  import useAuth from "../../hooks/useAuth";
+  import LogoContainer from "../../components/Form/LogoContainer";
+  import Logo from "../../assets/img/Logo.png";
+  import Footer from "../../components/Form/Footer";
+  
+  export default function Login() {
     const [loginData, setLoginData] = useState({ email: "", password: "" });
     const [isLoading, setIsLoading] = useState(false);
-    const { setToken } = useContext(AuthContext);
-
+    const { auth, login } = useAuth();
     const navigate = useNavigate();
-
+  
+    useEffect(() => {
+      if (auth && auth.token) {
+        navigate("/");
+      }
+    }, []);
+  
     function handleChange(e) {
-        setLoginData({ ...loginData, [e.target.name]: e.target.value });
+      setLoginData({ ...loginData, [e.target.name]: e.target.value });
     }
-
+  
     function handleSubmit(e) {
-        e.preventDefault();
-        setIsLoading(true);
-        const promise = axios.post("https://projeto--super-store.herokuapp.com/sign-in", {
-            email: loginData.email,
-            password: loginData.password
-        });
-        promise.then((resposta) => {
-            setToken(resposta.data);
-            navigate("/");
-        });
-        promise.catch((err) => {
-            setLoginData({ email: "", password: "" });
-            console.log("Deu ruim em", err);
+      e.preventDefault();
+  
+      setIsLoading(true);
+      const promise = api.postLogin({ ...loginData });
+      promise
+        .then((response) => {
+          setIsLoading(false);
+  
+          login(response.data);
+          navigate("/");
+        })
+        .catch((error) => {
+          setIsLoading(false);
+  
+          if (error.response.status === 422) {
+            setLoginData({
+              email: "",
+              password: "",
+            });
+            return Swal.fire({
+              icon: "error",
+              title: "Falha no login!",
+              text: "A senha deve ter no mínimo 6 caracteres e o e-mail deve ser válido.",
+            });
+          }
+  
+          if (error.response.status === 401) {
+            setLoginData({
+              email: "",
+              password: "",
+            });
+            return Swal.fire({
+              icon: "error",
+              title: "Falha no login!",
+              text: "E-mail e/ou senha incorretos.",
+            });
+          }
         });
     }
-
+  
     return (
-        <>
-            <LogoContainer>
-                <img src={Logo} alt="superstore-logo" onClick={() => navigate("/")} />
-            </LogoContainer>
-            <Container>
-                <Title>Faça seu login</Title>
-                <Form onSubmit={handleSubmit}>
-                    <Input
-                        type="email"
-                        placeholder="E-mail"
-                        name="email"
-                        onChange={handleChange}
-                        value={loginData.email}
-                        disabled={isLoading}
-                        required
-                    ></Input>
-                    <Input
-                        type="password"
-                        placeholder="Senha"
-                        name="password"
-                        onChange={handleChange}
-                        value={loginData.password}
-                        disabled={isLoading}
-                        required
-                    ></Input>
-                    <Button type="submit" disabled={isLoading}>
-                        {isLoading ? (
-                            <ThreeDots color="#FFFFFF" height={50} width={50} />
-                        ) : (
-                            "Entrar"
-                        )}
-                    </Button>
-                </Form>
-
-                <StyledLink to="/sign-up">
-                    <span>Primeira vez?</span>
-                    <span>Cadastre-se!</span>
-                </StyledLink>
-            </Container>
-            <Footer />
-        </>
+      <>
+        <LogoContainer>
+          <img src={Logo} alt="superstore-logo" onClick={() => navigate("/")} />
+        </LogoContainer>
+        <Container>
+          <Title>Faça seu login</Title>
+          <Form onSubmit={handleSubmit}>
+            <Input
+              type="email"
+              placeholder="E-mail"
+              name="email"
+              onChange={handleChange}
+              value={loginData.email}
+              disabled={isLoading}
+              required
+            ></Input>
+            <Input
+              type="password"
+              placeholder="Senha"
+              name="password"
+              onChange={handleChange}
+              value={loginData.password}
+              disabled={isLoading}
+              required
+            ></Input>
+            <Button type="submit" disabled={isLoading}>
+              {isLoading ? (
+                <ThreeDots color="#FFFFFF" height={50} width={50} />
+              ) : (
+                "Entrar"
+              )}
+            </Button>
+          </Form>
+  
+          <StyledLink to="/sign-up">
+            <span>Primeira vez?</span>
+            <span>Cadastre-se!</span>
+          </StyledLink>
+        </Container>
+        <Footer />
+      </>
     );
-}
+  }
