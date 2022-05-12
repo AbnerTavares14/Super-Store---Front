05282185f63630b3b5201dc55f5104facfd5b526
Abner <abnerdasilvatavares@gmail.com>
2022-05-12 14:17:22 -0300

    Correção do bug login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { GlobalStyle } from "./styles/style";
-import { MainPage, Login, SignUp, SingleProduct} from "./pages";
-import AuthContext from "./context/AuthContext";
-import CartContext from "./context/CartContext";
+import { MainPage, Login, SignUp, SingleProduct, Cart } from "./pages";
+import CartContext from "./context/cartContext";
+import AuthContext from "./context/authContext";
 
 
 export default function App() {
-    const [token, setToken] = useState(null);
+    const [auth, login] = useState(null);
     const [cartQuantity, setCartQuantity] = useState(0);
 
     return (
         <>
             <GlobalStyle />
             <BrowserRouter>
-            <CartContext.Provider value={{ cartQuantity, setCartQuantity }}>
-                <AuthContext.Provider value={{ token, setToken }}>
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/sign-up" element={<SignUp />} />
-                        <Route path="/product/:productId" element={<SingleProduct />} />
-                    </Routes>
-                </AuthContext.Provider>
+                <CartContext.Provider value={{ cartQuantity, setCartQuantity }}>
+                    <AuthContext.Provider value={{ auth, login }}>
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/sign-up" element={<SignUp />} />
+                            <Route path="/product/:productId" element={<SingleProduct />} />
+                            <Route path="/cart" element={<Cart />} />
+                        </Routes>
+                    </AuthContext.Provider>
                 </CartContext.Provider>
             </BrowserRouter>
         </>
--- src/services/api.js
+++ src/services/api.js
@@ -2,39 +2,39 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const BASE_URL = 123;//api_heroku aqui vai ser colocada a api do heroku
+const BASE_URL = "https://projeto--super-store.herokuapp.com";//api_heroku aqui vai ser colocada a api do heroku
 
 function postSignIn(body) {
     const promise = axios.post(`${BASE_URL}/sign-in`, body);
-  
+
     return promise;
 };
 
 function postSignUp(body) {
     const promise = axios.post(`${BASE_URL}/sign-up`, body);
-  
+
     return promise;
 };
 
 function getSingleProduct(productId) {
     const promise = axios.get(`${BASE_URL}/products/${productId}`);
-  
+
     return promise;
 };
 
 function postAddToCart(body, token) {
     const promise = axios.post(`${BASE_URL}/add-to-cart`, body, token);
-  
+
     return promise;
 }
 
 
 const api = {
     postSignUp,
-    postSignUp,
     getSingleProduct,
-    postAddToCart
+    postAddToCart,
+    postSignIn
 };
-  
+
 export default api;
 
